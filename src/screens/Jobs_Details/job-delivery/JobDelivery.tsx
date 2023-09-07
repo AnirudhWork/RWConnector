e89c7281b654a4de2423086362ab1fafba4fc021
AnirudhWork <anirudh_m@trigent.com>
2023-09-07 18:08:34 +0530

    fixes

--- src/screens/Jobs_Details/job-delivery/JobDelivery.tsx
+++ src/screens/Jobs_Details/job-delivery/JobDelivery.tsx
@@ -1,11 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import { IJobDeliveryDetailsProps } from '../../types'
+import moment, { unix } from 'moment';
+import { JOB_DETAILS_NOTE } from '../job-details-constants';
 
 const JobDelivery: React.FC<IJobDeliveryDetailsProps> = ( { jobDetailsData } ) => {
+
+    const [endDate, setEndDate] = useState<string>();
+    const deliveryDate = jobDetailsData?.['del-date'];
+
+    useEffect( () => {
+        setEndDate(
+            deliveryDate
+                ? moment( unix( deliveryDate ) ).format( 'DD-MMM-YYYY' )
+                : '',
+        );
+    }, [] )
     return (
         <View style={styles.container}>
-            <Text>{jobDetailsData?.['del-note']}</Text>
+            {jobDetailsData && (
+                <ScrollView automaticallyAdjustKeyboardInsets={true}>
+                    {/* Date section */}
+                    <View style={styles.numberInfoContainer}>
+                        <Text>
+                            {jobDetailsData['bol-num']}
+                        </Text>
+                        <View>
+                            <Text>
+                                {endDate}
+                            </Text>
+                            <Text>
+                                {JOB_DETAILS_NOTE.FIXED}
+                            </Text>
+                        </View>
+                    </View>
+                </ScrollView>
+            )}
         </View>
     )
 }
@@ -13,9 +43,13 @@ const JobDelivery: React.FC<IJobDeliveryDetailsProps> = ( { jobDetailsData } ) =
 const styles = StyleSheet.create( {
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+        backgroundColor: '#FFFFFF',
+    },
+    numberInfoContainer: {
+        flexDirection: 'column',
+        backgroundColor: 'transparent',
+        padding: 15,
+    },
 } )
 
 
--- src/screens/Jobs_Details/job-details-constants.ts
+++ src/screens/Jobs_Details/job-details-constants.ts
@@ -2,3 +2,7 @@ export enum TAB_NAVIGATOR_SCREEN {
     PICKUP = 'PICKUP',
     DELIVERY = 'DELIVERY',
 }
+
+export enum JOB_DETAILS_NOTE {
+    FIXED = 'MC 555925 USDOT 1471933'
+}
